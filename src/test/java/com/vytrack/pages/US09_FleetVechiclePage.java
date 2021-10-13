da53495d47af9a612d625ba7db5bfd1f3704bf6c
YuliiaMashkina <yuliiamashkina@gmail.com>
2021-10-13 00:31:24 -0400

    Completed User Story 9

--- src/test/java/com/vytrack/pages/US09_FleetVechiclePage.java
+++ src/test/java/com/vytrack/pages/US09_FleetVechiclePage.java
@@ -1,7 +1,10 @@
 package com.vytrack.pages;
 
+import com.github.javafaker.Faker;
 import com.trycloud.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -16,6 +19,91 @@ public class US09_FleetVechiclePage {
 
     @FindBy (xpath = "//span[@class='title title-level-2' and normalize-space(.)='Vehicle Contracts']")
     public WebElement vehicleContract;
+
+    @FindBy (xpath = "//a[@class='btn main-group btn-primary pull-right ']")
+    public WebElement createBtn;
+
+    @FindBy (xpath = "(//span[@class='select2-arrow'])[1]")
+    public WebElement typeDrop;
+
+    @FindBy (xpath = "//div[@class='select2-result-label' and .='Cash']")
+    public WebElement cashOption;
+
+    @FindBy (xpath = "//div[@id='flash-messages']/div/div[@class='flash-messages-holder']")
+    public WebElement confirmationMessage;
+
+    @FindBy (xpath = "//button[@class='btn btn-success action-button' and normalize-space(.)='Save and Close']")
+    public WebElement saveButton;
+
+
+    //contractInfo
+    @FindBy (name = "custom_entity_type[Responsible]")
+    public WebElement responsible;
+
+    @FindBy (name = "custom_entity_type[ActivationCost]")
+    public WebElement activationCost;
+
+    @FindBy (name = "custom_entity_type[RecurringCostAmount]")
+    public WebElement costAmount;
+
+    @FindBy (xpath = "(//span[@class='select2-arrow'])[2]")
+    public WebElement costDrop;
+
+    @FindBy (xpath = "//div[@class='select2-result-label' and .='Daily']")
+    public WebElement dailyOption;
+
+    @FindBy (name = "custom_entity_type[OdometerDetails]")
+    public WebElement odometerDetails;
+
+    @FindBy (name = "custom_entity_type[Vendor]")
+    public WebElement vendor;
+
+    @FindBy (name = "custom_entity_type[Driver]")
+    public WebElement driver;
+
+    @FindBy (name = "custom_entity_type[ContractReference]")
+    public WebElement contactReference;
+
+    @FindBy (name = "custom_entity_type[TermsandConditions]")
+    public WebElement termsAndConditions;
+
+    @FindBy (xpath = "(//span[@class='select2-arrow'])[3]")
+    public WebElement statusDrop;
+
+    @FindBy (xpath = "//div[@class='select2-result-label' and .='Active']")
+    public WebElement activeOption;
+
+
+
+    public void fillUpContract() {
+        Actions actions = new Actions(Driver.getDriver());
+        Faker faker = new Faker();
+
+        typeDrop.click();
+        actions.moveToElement(cashOption).click().perform();
+        responsible.sendKeys(faker.name().fullName());
+        activationCost.sendKeys(faker.number().digits(2));
+        costAmount.sendKeys(faker.number().digits(3));
+        costDrop.click();
+        actions.moveToElement(dailyOption).click().perform();
+        odometerDetails.sendKeys(faker.number().digits(6));
+       /* WebElement invoiceDate = Driver.getDriver().findElement(By.xpath("(//input[@placeholder='Choose a date'])[1]"));
+        //invoiceDate.sendKeys(faker.numerify("00/00"));
+
+        WebElement contractStartDate = Driver.getDriver().findElement(By.xpath("(//input[@placeholder='Choose a date'])[2]"));
+        //contractStartDate.sendKeys(faker.date().future());
+
+        WebElement contractExpirationDate = Driver.getDriver().findElement(By.xpath("(//input[@placeholder='Choose a date'])[3]"));
+        //contractExpirationDate.sendKeys(faker.numerify("00/00"));*/
+        vendor.sendKeys(faker.name().fullName());
+        driver.sendKeys(faker.name().fullName());
+        contactReference.sendKeys(faker.name().fullName());
+        termsAndConditions.sendKeys(faker.book().toString());
+        statusDrop.click();
+        actions.moveToElement(activeOption).click().perform();
+    }
+
+
 
 
 }
--- src/test/java/com/vytrack/tests/UserStory9/US9_CreateVehicleContract.java
+++ src/test/java/com/vytrack/tests/UserStory9/US9_CreateVehicleContract.java
@@ -4,6 +4,7 @@ import com.github.javafaker.Faker;
 import com.trycloud.utilities.BrowserUtils;
 import com.trycloud.utilities.ConfigurationReader;
 import com.trycloud.utilities.Driver;
+import com.trycloud.utilities.TestBase;
 import com.vytrack.pages.LoginPage;
 import com.vytrack.pages.US09_FleetVechiclePage;
 import org.junit.jupiter.api.Assertions;
@@ -17,10 +18,10 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.List;
 
 
-public class US9_CreateVehicleContract {
+public class US9_CreateVehicleContract extends TestBase {
 
     @Test
-    public void createContract() {
+    public void storeManager1CreateContract() {
         LoginPage login = new LoginPage();
         login.goTo();
         login.login(ConfigurationReader.read("storeManager1"), ConfigurationReader.read("password"));
@@ -33,30 +34,44 @@ public class US9_CreateVehicleContract {
                 .moveToElement(page.vehicleContract).pause(1000)
                 .click().perform();
 
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 2);
+        wait.until(ExpectedConditions.titleIs("All - Vehicle Contract - Entities - System - Car - Entities - System"));
+        page.createBtn.click();
+
+        BrowserUtils.waitFor(2);
+
+        page.fillUpContract();
+        page.saveButton.click();
+        BrowserUtils.waitFor(2);
+
+       Assertions.assertTrue(page.confirmationMessage.isDisplayed());
+    }
+
+    @Test
+    public void salesManager1CreateContract() {
+        LoginPage login = new LoginPage();
+        login.goTo();
+        login.login(ConfigurationReader.read("salesManager1"), ConfigurationReader.read("password"));
+        BrowserUtils.waitFor(2);
+
+        US09_FleetVechiclePage page = new US09_FleetVechiclePage();
+
+        Actions actions = new Actions(Driver.getDriver());
+        actions.moveToElement(page.fleetModule).pause(1000)
+                .moveToElement(page.vehicleContract).pause(1000)
+                .click().perform();
 
         WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 2);
         wait.until(ExpectedConditions.titleIs("All - Vehicle Contract - Entities - System - Car - Entities - System"));
-
-
-        WebElement createBtn = Driver.getDriver().findElement(By.xpath("//a[@class='btn main-group btn-primary pull-right ']"));
-        createBtn.click();
-
-        BrowserUtils.waitFor(2);
-        WebElement typeDrop = Driver.getDriver().findElement(By.xpath("(//span[@class='select2-arrow'])[1]"));
-        typeDrop.click();
-
-        WebElement cashOption = Driver.getDriver().findElement(By.xpath("//div[@class='select2-result-label' and .='Cash']"));
-        actions.moveToElement(cashOption).click().perform();
-
-        fillUpContract();
-
-        WebElement saveButton = Driver.getDriver().findElement(By.xpath("//button[@class='btn btn-success action-button' and normalize-space(.)='Save and Close']"));
-        saveButton.click();
+        page.createBtn.click();
 
         BrowserUtils.waitFor(2);
 
-        WebElement confirmationMessage = Driver.getDriver().findElement(By.xpath("//div[@id='flash-messages']/div/div[@class='flash-messages-holder']"));
-        Assertions.assertTrue(confirmationMessage.isDisplayed());
+        page.fillUpContract();
+        page.saveButton.click();
+        BrowserUtils.waitFor(2);
+
+        Assertions.assertTrue(page.confirmationMessage.isDisplayed());
     }
 
     @Test
@@ -66,98 +81,31 @@ public class US9_CreateVehicleContract {
         login.login(ConfigurationReader.read("truckDriver1"), ConfigurationReader.read("password"));
         BrowserUtils.waitFor(2);
 
-        WebElement fleetModule = Driver.getDriver().findElement(By.xpath("//span[@class='title title-level-1' and normalize-space(.)='Fleet']"));
-        WebElement vehicleContract = Driver.getDriver().findElement(By.xpath("//span[@class='title title-level-2' and normalize-space(.)='Vehicle Contracts']"));
+        US09_FleetVechiclePage page = new US09_FleetVechiclePage();
 
         Actions actions = new Actions(Driver.getDriver());
-        actions.moveToElement(fleetModule).pause(1000)
-                .moveToElement(vehicleContract).pause(1000)
+        actions.moveToElement(page.fleetModule).pause(1000)
+                .moveToElement(page.vehicleContract).pause(1000)
                 .click().perform();
 
         WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 2);
         wait.until(ExpectedConditions.titleIs("Dashboard"));
 
-        //WebElement createBtn=Driver.getDriver().findElement(By.xpath("//a[@class='btn main-group btn-primary pull-right ']"));
-        //Assertions.assertTrue(createBtn.isDisplayed());
-
-      /*  try {
-            boolean createBtn1 =Driver.getDriver().findElement(By.xpath("//a[@class='btn main-group btn-primary pull-right ']")).isDisplayed();
-        }
-        catch (org.openqa.selenium.NoSuchElementException e) {
-            return;
-        }*/
-
-       /* try {
-            System.out.println("Driver.getDriver().findElement(By.xpath(\"//a[@class='btn main-group btn-primary pull-right ']\")).isDisplayed() = " + Driver.getDriver().findElement(By.xpath("//a[@class='btn main-group btn-primary pull-right ']")).isDisplayed());
-        } catch (NoSuchElementException e) {
-
-        }
-        Assertions.assertTrue(Driver.getDriver().findElement(By.xpath("//a[@class='btn main-group btn-primary pull-right ']")) == null);
-       // Assertions.assertTrue(Driver.getDriver().findElement(By.xpath("//a[@class='btn main-group btn-primary pull-right ']")).isDisplayed());
-*/
-
-        //Assertions.assertTrue(createBtn=NoSuchElementException.class);
-        //assertTrue(createBtn.isEmpty());
-
-        List<WebElement> createBtn = Driver.getDriver().findElements(By.xpath("//a[@class='btn main-group btn-primary pull-right ']"));
-        if (createBtn.size() == 0) {
-            //System.out.println("Element is not visible");
-            Assertions.assertTrue(createBtn.size()==0);
-        } else {
-            System.out.println("Element is visible");
-        }
+//        List<WebElement> createBtn = Driver.getDriver().findElements(By.xpath("//a[@class='btn main-group btn-primary pull-right ']"));
+//        if (createBtn.size() == 0) {
+//            //System.out.println("Element is not visible");
+//            Assertions.assertTrue(createBtn.size()==0);
+//        } else {
+//            System.out.println("Element is visible");
+//        }
+       // BrowserUtils.checkVisibilityOfElement(By.xpath("//a[@class='btn main-group btn-primary pull-right ']"), 2);
+        Assertions.assertTrue(BrowserUtils.checkVisibilityOfElement(By.xpath("//a[@class='btn main-group btn-primary pull-right ']"), 2));
+//finish
 
 
 
     }
 
-    public void fillUpContract() {
-        Actions actions = new Actions(Driver.getDriver());
-        Faker faker = new Faker();
-
-        WebElement responsible = Driver.getDriver().findElement(By.name("custom_entity_type[Responsible]"));
-        responsible.sendKeys(faker.name().fullName());
-
-        WebElement activationCost = Driver.getDriver().findElement(By.name("custom_entity_type[ActivationCost]"));
-        activationCost.sendKeys(faker.number().digits(2));
-
-        WebElement costAmount = Driver.getDriver().findElement(By.name("custom_entity_type[RecurringCostAmount]"));
-        costAmount.sendKeys(faker.number().digits(3));
-
-        WebElement costDrop = Driver.getDriver().findElement(By.xpath("(//span[@class='select2-arrow'])[2]"));
-        costDrop.click();
-        WebElement dailyOption = Driver.getDriver().findElement(By.xpath("//div[@class='select2-result-label' and .='Daily']"));
-        actions.moveToElement(dailyOption).click().perform();
-
-        WebElement odometerDetails = Driver.getDriver().findElement(By.name("custom_entity_type[OdometerDetails]"));
-        odometerDetails.sendKeys(faker.number().digits(6));
-
-        WebElement invoiceDate = Driver.getDriver().findElement(By.xpath("(//input[@placeholder='Choose a date'])[1]"));
-        //invoiceDate.sendKeys(faker.numerify("00/00"));
-
-        WebElement contractStartDate = Driver.getDriver().findElement(By.xpath("(//input[@placeholder='Choose a date'])[2]"));
-        //contractStartDate.sendKeys(faker.date().future());
-
-        WebElement contractExpirationDate = Driver.getDriver().findElement(By.xpath("(//input[@placeholder='Choose a date'])[3]"));
-        //contractExpirationDate.sendKeys(faker.numerify("00/00"));
-
-        WebElement vendor = Driver.getDriver().findElement(By.name("custom_entity_type[Vendor]"));
-        vendor.sendKeys(faker.name().fullName());
-
-        WebElement driver = Driver.getDriver().findElement(By.name("custom_entity_type[Driver]"));
-        driver.sendKeys(faker.name().fullName());
-
-        WebElement contactReference = Driver.getDriver().findElement(By.name("custom_entity_type[ContractReference]"));
-        contactReference.sendKeys(faker.name().fullName());
-
-        WebElement termsAndConditions = Driver.getDriver().findElement(By.name("custom_entity_type[TermsandConditions]"));
-        termsAndConditions.sendKeys(faker.book().toString());
-
-        WebElement statusDrop = Driver.getDriver().findElement(By.xpath("(//span[@class='select2-arrow'])[3]"));
-        statusDrop.click();
-        WebElement activeOption = Driver.getDriver().findElement(By.xpath("//div[@class='select2-result-label' and .='Active']"));
-        actions.moveToElement(activeOption).click().perform();
-    }
 
 
 
